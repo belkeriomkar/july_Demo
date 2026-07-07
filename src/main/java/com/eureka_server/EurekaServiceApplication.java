272ec9a92da33e0acb6ce2ff2d324578c69cb84a
OMKAR <134843430+belkeriomkar@users.noreply.github.com>
2026-07-07 20:12:44 +0530

    Add new fields to EurekaServiceApplication class

--- src/main/java/com/eureka_server/EurekaServiceApplication.java
+++ src/main/java/com/eureka_server/EurekaServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication {
-
+String name="mike";
+	int x=100;
+	boolean isPresent=false;
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
